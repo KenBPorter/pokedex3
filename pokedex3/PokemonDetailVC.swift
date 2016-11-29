e22cbb041207fd2984d8a6f4ad6ebce203cb969a
Ken <porterk@kensmbp.local>
2016-11-29 16:40:22 -0500

    L113 - lots of @IBOutlets - last stop before addition of data parsing and Alamofire

--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -14,6 +14,20 @@ class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
     
     
     // the variabe that receives the passed in poke object
@@ -31,11 +45,7 @@ class PokemonDetailVC: UIViewController {
     // @IB Actions //
 
     @IBAction func backButtonPressed(_ sender: UIButton) {
-        
         dismiss(animated: true, completion: nil)
     }
-    
-    
-
     
 }
